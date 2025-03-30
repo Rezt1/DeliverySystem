d99fe14b18fa9f232df9cc308bfcd1b748b13703
AllyMihaylova <alimihailova04@gmail.com>
2025-03-30 21:10:39 +0300

    filter by rating and chefs kiss implemented.
    Дости сорка ма трябваше да барна по css за chefs picks че изглеждаше мега гадно като ги слагах ако нещо не те кефи може да го оправиш

--- frontend/src/restaurantsJs.js
+++ frontend/src/restaurantsJs.js
@@ -6,7 +6,9 @@ async function fetchingRestaurants(){
     
     }
 
-    export async function displayDashBoard(){
+        let btnRatingSort = document.getElementById("filter-button");
+        btnRatingSort.addEventListener("click", filterByRating);
+
 
         let data = await fetchingRestaurants();
         
@@ -30,5 +32,44 @@ async function fetchingRestaurants(){
             restaurantsList.appendChild(div);
         });
       
+        chefsPick();
+
+      async function filterByRating(){
+
+        data.sort((a, b) => b.rating - a.rating);
+
+        restaurantsList.innerHTML = '';
+
+        data.forEach(data => {
+            let div = document.createElement("div");
+            div.classList.add("restaurant-item", "bg-white", "rounded-lg", "shadow-md", "p-4");
+            div.innerHTML = `<strong>${data.name}</strong> <br> Rating: ⭐${data.rating}`;
+            restaurantsList.appendChild(div);
+        });
+
       }
-      displayDashBoard();
+
+    async function  chefsPick() {
+        data.sort((a, b) => b.rating - a.rating);
+        
+        let loading1 = document.getElementById("loading-1");
+        let loading2 = document.getElementById("loading-2");
+
+        loading1.classList.add("hidden");
+        loading2.classList.add("hidden");
+
+        let buttonChef1 = document.getElementById("chef-pick-1");
+        buttonChef1.classList.remove('text-transparent'); 
+        buttonChef1.classList.add('text-black');  
+        buttonChef1.disabled = false;
+
+        buttonChef1.textContent =  `${data[0].name} - Rating: ⭐${data[0].rating}`;
+
+        let buttonChef2 = document.getElementById("chef-pick-2");
+        buttonChef2.classList.remove('text-transparent'); 
+        buttonChef2.classList.add('text-black');  
+        buttonChef2.disabled = false;
+
+        buttonChef2.textContent =  `${data[1].name} - Rating: ⭐${data[1].rating}`;
+
+    }
