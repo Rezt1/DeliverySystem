7d8855953ab5a957e43cb7eed0bf112fb9c50154
AllyMihaylova <alimihailova04@gmail.com>
2025-03-31 09:37:41 +0300

    folder change node.js server

---
+++ frontend/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/", express.static("./static"));
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Server running");
+})
