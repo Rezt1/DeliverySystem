9e71fd40f242aa7473dbd1099934cc1e25348534
AllyMihaylova <alimihailova04@gmail.com>
2025-04-01 15:05:11 +0300

    login + logout now work

--- frontend/index.js
+++ frontend/index.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use("/", express.static("./static"));
 
-app.listen(process.env.PORT || 8081, () => {
+app.listen(process.env.PORT || 8082, () => {
     console.log("Server running");
 })
