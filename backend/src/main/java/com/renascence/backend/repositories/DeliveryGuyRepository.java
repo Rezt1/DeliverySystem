6d26d143fa0cdf0e59c063b63a29a34aa2ea8786
Velizara Sotirova <114175948+VelizaraSotirova@users.noreply.github.com>
2025-04-06 15:59:09 +0300

    roles in security config added

--- backend/src/main/java/com/renascence/backend/repositories/DeliveryGuyRepository.java
+++ backend/src/main/java/com/renascence/backend/repositories/DeliveryGuyRepository.java
@@ -2,10 +2,7 @@ package com.renascence.backend.repositories;
 
 import com.renascence.backend.entities.DeliveryGuy;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface DeliveryGuyRepository extends JpaRepository<DeliveryGuy, Long> {
